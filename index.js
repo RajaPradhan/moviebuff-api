dc364aa773b822d83119729fc119fe4e5ccdbd58
RajaPradhan <rajapradhan08@gmail.com>
2017-09-24 07:44:48 +0530

    Add CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const config = require('./config');
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
